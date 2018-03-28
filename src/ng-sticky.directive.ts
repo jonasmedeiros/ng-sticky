d145aa839871a579d9bf819cde85544e8fdec8b5
Matt Blum <matt@nationalmachine.co>
2018-03-28 09:01:05 -0500

    add ability for directive to determine offset from the parent

--- src/ng-sticky.directive.ts
+++ src/ng-sticky.directive.ts
@@ -1,53 +1,58 @@
-import { Directive, ElementRef, Input, HostListener, Renderer2 } from '@angular/core';
+import {Directive, ElementRef, Input, HostListener, Renderer2} from "@angular/core";
 
 @Directive({
-  selector: '[ng-sticky]'
+    selector: "[ng-sticky]"
 })
 export class NgStickyDirective {
 
-  private sticked: boolean = true;
-  private selectedOffset: number = 0;
-  private windowOffsetTop: number = 0;
+    private sticked: boolean = true;
+    private selectedOffset: number = 0;
+    private windowOffsetTop: number = 0;
 
-  @Input() addClass: string = 'fixed';
-  @Input() offSet: number = 0;
+    @Input() addClass: string = "fixed";
+    @Input() offSet: number = 0;
+    @Input() getOffsetTopFromParent = false;
 
-  constructor(private el: ElementRef, private render:Renderer2) {
-    this.selectedOffset = this.el.nativeElement.offsetTop;
-  }
+    constructor(private el: ElementRef, private render: Renderer2) {
+        this.selectedOffset = this.el.nativeElement.offsetTop;
+    }
 
-  private addSticky() {
-    this.sticked = true;
-    this.el.nativeElement.style.position = 'fixed';
-    this.el.nativeElement.style.top = this.offSet + 'px';
-    this.render.addClass(this.el.nativeElement, this.addClass);
-  }
+    private addSticky() {
+        this.sticked = true;
+        this.el.nativeElement.style.position = "fixed";
+        this.el.nativeElement.style.top = this.offSet + "px";
+        this.render.addClass(this.el.nativeElement, this.addClass);
+    }
 
-  private removeSticky() {
-    this.sticked = false;
-    this.el.nativeElement.style.position = '';
-    this.render.removeClass(this.el.nativeElement, this.addClass);
-  }
+    private removeSticky() {
+        this.sticked = false;
+        this.el.nativeElement.style.position = "";
+        this.render.removeClass(this.el.nativeElement, this.addClass);
+    }
 
-  @HostListener("window:scroll", [])
+    private offsetIsDerivedFromParent() {
+        return this.getOffsetTopFromParent && this.el.nativeElement.offsetParent;
+    }
+
+    @HostListener("window:scroll", [])
     onWindowScroll() {
 
-      let offset: number = this.el.nativeElement.offsetTop;
-      this.windowOffsetTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        const offset = this.offsetIsDerivedFromParent() ? this.el.nativeElement.offsetParent.offsetTop : this.el.nativeElement.offsetTop;
+        this.windowOffsetTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-      if (this.selectedOffset === 0) {
-        this.selectedOffset = offset;
-      }
+        if (this.selectedOffset === 0) {
+            this.selectedOffset = offset;
+        }
 
-      if (this.sticked === false) {
-        this.selectedOffset = offset;
-      }
+        if (this.sticked === false) {
+            this.selectedOffset = offset;
+        }
 
-      if ((this.windowOffsetTop + this.offSet) > this.selectedOffset) {
-        
-        this.addSticky();
-      } else {
-        this.removeSticky();
-      }
+        if ((this.windowOffsetTop + this.offSet) > this.selectedOffset) {
+
+            this.addSticky();
+        } else {
+            this.removeSticky();
+        }
     }
 }
